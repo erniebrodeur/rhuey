b9fe10a7ce28c751ae5d4831bcafd00554d3b2b9
Ernie Brodeur <ebrodeur@ujami.net>
2022-02-07 00:49:20 -0800

    init commit

---
+++ lib/rhuey.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rhuey/version"
+
+module Rhuey
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rhuey_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rhuey do
+  it "has a version number" do
+    expect(Rhuey::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
